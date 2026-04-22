895a0623e67462e92a4c86d7c024199480e56442
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-04-23 00:01:02 +1000

    fish: skim keybindings

--- fish/.config/fish/conf.d/00-env.fish
+++ fish/.config/fish/conf.d/00-env.fish
@@ -5,7 +5,3 @@ set -gx DOTFILES $HOME/.dotfiles
 
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
-
-set -gx SKIM_DEFAULT_COMMAND "fd --hidden --strip-cwd-prefix --exclude .git"
-set -gx SKIM_CTRL_T_COMMAND $SKIM_DEFAULT_COMMAND
-set -gx SKIM_ALT_C_COMMAND "fd --type=d --hidden --strip-cwd-prefix --exclude .git"
